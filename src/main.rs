862733aa99476cdc0e899ab85d49083a85593b8a
elfstead <me@elfstead.com>
2023-05-11 01:18:33 +0200

    normal render pipeline (gonna change to compute)

---
+++ src/main.rs
@@ -0,0 +1,201 @@
+use std::time::{Duration, Instant};
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    window::{Window, WindowBuilder},
+};
+
+struct State {
+    window: Window,
+    size: winit::dpi::PhysicalSize<u32>,
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    surface_config: wgpu::SurfaceConfiguration,
+    render_pipeline: wgpu::RenderPipeline,
+}
+
+impl State {
+    async fn new(window: Window) -> Self {
+        let size = window.inner_size();
+
+        // Instance of wgpu.
+        // Its primary use is to create Adapters and Surfaces.
+        // Does not have to be kept alive.
+        let instance = wgpu::Instance::new(Default::default());
+
+        // Draw to this surface, based on a raw window handle
+        let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+        // Handle to a physical graphics and/or compute device.
+        // Used for request_device(), then not needed.
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::HighPerformance,
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        // Actual connection to the GPU
+        let (device, queue) = adapter
+            .request_device(&Default::default(), None)
+            .await
+            .unwrap();
+
+        // This is needed for color format, size, alpha, other stuff
+        let surface_config = surface
+            .get_default_config(&adapter, size.width, size.height)
+            .unwrap();
+
+        surface.configure(&device, &surface_config);
+
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: surface_config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTop,
+            },
+        });
+
+        State {
+            window,
+            size,
+            surface,
+            device,
+            queue,
+            surface_config,
+            render_pipeline,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.surface_config.width = new_size.width;
+            self.surface_config.height = new_size.height;
+            self.surface.configure(&self.device, &self.surface_config);
+        }
+    }
+
+    fn update(&mut self, dt: Duration) {
+        //todo
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                // Need a color attachment linked to our view (which is linked to the screen)
+                // so that colors drawn during render end up on the screen
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
+
+async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut state = State::new(window).await;
+    let mut last_render_time = Instant::now();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent { ref event, .. } => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => control_flow.set_exit(),
+            WindowEvent::Resized(physical_size) => {
+                state.resize(*physical_size);
+            }
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                state.resize(**new_inner_size);
+            }
+            _ => (),
+        },
+        Event::MainEventsCleared => {
+            state.window.request_redraw();
+        }
+        Event::RedrawRequested(_) => {
+            let now = Instant::now();
+            let dt = now - last_render_time;
+            last_render_time = now;
+            state.update(dt);
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if it's lost or outdated
+                Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                    state.resize(state.size)
+                }
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => control_flow.set_exit(),
+                // We're ignoring timeouts
+                Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+            }
+        }
+        _ => (),
+    });
+}
+
+fn main() {
+    pollster::block_on(run());
+}
---
+++ src/shader.wgsl
@@ -0,0 +1,5 @@
+@compute
+fn fs_main(@builtin(position) pos) -> @location(0) vec4<f32> {
+    
+    return vec4f(0.75, 0.5, 0.25, 1);
+}
