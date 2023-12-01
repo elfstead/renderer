d600f582a52c47c4f6948886202956e9922334ca
elfstead <me@elfstead.com>
2023-12-01 02:52:05 +0100

    remove old print debuggin code

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,3 @@
-use glam::{vec3, Mat3, Vec3};
 use std::path::Path;
 
 use wgpu::util::DeviceExt;
@@ -140,11 +139,6 @@ pub fn load<P: AsRef<Path>>(
     let mut index_offset = 0;
     for m in &obj_models {
         let mat_id = m.mesh.material_id.expect("mesh missing material id");
-        let ccc = obj_materials[mat_id].diffuse.unwrap();
-        println!(
-            "{} {} MAT ID {} {} {}",
-            m.name, mat_id, ccc[0], ccc[1], ccc[2]
-        );
         mesh_info.push(MeshInfo {
             vertex_offset,
             index_offset,
@@ -178,87 +172,11 @@ pub fn load<P: AsRef<Path>>(
         _padding2: 0,
     });
 
-    for c in &colors {
-        let ccc = c.diffuse_color;
-        println!("{} {} {}", ccc[0], ccc[1], ccc[2]);
-    }
-
     let indices = obj_models
         .iter()
         .map(|m| m.mesh.indices.clone()) // should avoid unnecessary clone
         .flatten()
         .collect::<Vec<u32>>();
-
-    const SCREEN_W: usize = 25;
-    const SCREEN_H: usize = 25;
-    let mut screendraw = [[vec3(0.0, 0.0, 0.0); SCREEN_H]; SCREEN_W];
-
-    let cam_r = Mat3::IDENTITY;
-    let ro = vec3(250.0, 250.0, -500.0);
-
-    for screen_y in 0..SCREEN_H {
-        for screen_x in 0..SCREEN_W {
-            let mut distance: f32 = 1e20;
-            let mut color = vec3(0.0, 0.0, 0.0);
-            let px_vec = vec3(
-                screen_x as f32 - SCREEN_W as f32 / 2.0,
-                screen_y as f32 - SCREEN_H as f32 / 2.0,
-                SCREEN_H as f32 / 2.0,
-            );
-
-            let rd = cam_r * px_vec;
-
-            for i in 0..(mesh_info.len() - 1) {
-                let v_offs = mesh_info[i].vertex_offset;
-                let i_offs = mesh_info[i].index_offset;
-                let i_end = mesh_info[i + 1].index_offset;
-                for j in i_offs / 3..i_end / 3 {
-                    let v0: Vec3 = vertices[v_offs as usize + indices[j as usize * 3] as usize]
-                        .position
-                        .into();
-                    let v1: Vec3 = vertices[v_offs as usize + indices[j as usize * 3 + 1] as usize]
-                        .position
-                        .into();
-                    let v2: Vec3 = vertices[v_offs as usize + indices[j as usize * 3 + 2] as usize]
-                        .position
-                        .into();
-
-                    /*
-                    println!(
-                        "index: {}, vertex: {} {} {}, vo: {}",
-                        indices[j as usize * 3],
-                        v0.x,
-                        v0.y,
-                        v0.z,
-                        v_offs
-                    );
-                    */
-
-                    let e1 = v1 - v0;
-                    let e2 = v2 - v0;
-                    let b = ro - v0;
-
-                    let n = Vec3::cross(e1, e2);
-                    let q = Vec3::cross(b, rd);
-
-                    let d = 1.0 / Vec3::dot(rd, n);
-                    let u = d * Vec3::dot(-q, e2);
-                    let v = d * Vec3::dot(q, e1);
-                    let t = d * Vec3::dot(-n, b);
-
-                    if u >= 0.0 && v >= 0.0 && u + v <= 1.0 && t > 0.00001 {
-                        if distance > t * rd.length() {
-                            distance = t * rd.length();
-                            color = vec3(1.0, 1.0, 1.0);
-                        }
-                    }
-                }
-            }
-            screendraw[screen_x][screen_y] = color;
-            print!("({}, {}, {})", color[0], color[1], color[2]);
-        }
-        println!();
-    }
 
     let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("Vertex Buffer"),
